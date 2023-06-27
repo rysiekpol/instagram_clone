84f35c578f82027ca5c4f7bc9ec94ac00d68d1c8
Maciej Szefler <maciej.szefler@profil-software.com>
2023-06-27 10:51:24 +0200

    Add user board photo display

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,9 +1,32 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
-from wtforms import SubmitField, TextAreaField, FileField
-from wtforms.validators import DataRequired
+from wtforms import SubmitField, TextAreaField, FileField, StringField
+from wtforms.validators import DataRequired, Length, ValidationError
+from app.models import User
+from flask import flash
 
 class UploadForm(FlaskForm):
     photo = FileField('Upload Photo', validators=[FileRequired('File field should not be empty'), FileAllowed(['jpg', 'png'], 'Images only!')])
     description = TextAreaField('Describe your image', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me',
+                             validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                flash('Please use a different username.')
+                raise ValidationError('Please use a different username.')
+
+
+class EmptyForm(FlaskForm):
     submit = SubmitField('Submit')
